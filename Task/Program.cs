0e0b3edb252fd785c914548ae8d1011ec6286af1
Maikl <umnov.mixail@mail.ru>
2023-10-04 14:43:29 +0400

    в папку FINAL WORK добавели алгоритм программы в папке Task  + в файл README добавили путь к программе(17)

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿void FIllArray(string [] array, string[] array22)
+{    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <=3 )
+        {
+          array22[count] = array[i];
+          count++; 
+        }
+    }
+}
+
+void PrintArray(string []array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"{array[i]} ");
+}
+System.Console.WriteLine();
+}
+
+
+string [] array10 = new string [7] {"#42", "gfdgs", "#$%@", "76", "the", "1", "FG"} ;
+string [] array22 = new string[array10.Length];
+FIllArray(array10,array22);
+PrintArray(array22);
